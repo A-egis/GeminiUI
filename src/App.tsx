d43fface91a3c1a328def1d234b7f2e6fb1d5b73
dengsinan <1754510323@qq.com>
2021-03-29 19:33:23 +0800

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from "react";
-
+import Button, { ButtonSize, ButtonType } from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+import SubMenu from "./components/Menu/subMenu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <FontAwesomeIcon icon={faCoffee} size="lg" />
+        <Menu
+          onSelect={(index) => {
+            console.log(index);
+          }}
+          // mode="vertical"
+          defaultOpenSubMenus={["3"]}
         >
-          Learn React
-        </a>
-        <h1>Hello world!</h1>
-        <h2>hello world!</h2>
-        <hr />
-        <code>const a = 'b'</code>
+          <MenuItem>link 1</MenuItem>
+          <MenuItem disabled>link 2</MenuItem>
+          <MenuItem>link 3</MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>dropdown1</MenuItem>
+            <MenuItem>dropdown2</MenuItem>
+          </SubMenu>
+        </Menu>
+        {/* <Button
+          autoFocus
+          onClick={(e) => {
+            e.preventDefault();
+            alert("123");
+          }}
+        >
+          123
+        </Button>
+        <Button
+          className="custom"
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        >
+          123
+        </Button>
+        <Button btnType={ButtonType.Link} href="https://baidu.com/" disabled>
+          Baidu.com
+        </Button>
+        <Button btnType={ButtonType.Danger} disabled>
+          123
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="https://baidu.com/"
+          target="_blank"
+        >
+          Baidu.com
+        </Button> */}
       </header>
     </div>
   );
